8a26ab870fb5176672f9d9b17639a90c71657f4d
samyon196 <samyon196@mail.com>
2020-04-02 15:05:46 +0300

    aaa

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express()
 app.use(cors())
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Still cant upload your app? :O'))
 
 app.listen(port, () => console.log(`Example app listening at :${port}`))
