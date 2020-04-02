c13751c73e1335c8ed4a30e14afd56303af0624e
samyon196 <samyon196@mail.com>
2020-04-02 14:56:26 +0300

    port fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 var cors = require('cors')
 var app = express()
 app.use(cors())
-const port = 3000 | process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
