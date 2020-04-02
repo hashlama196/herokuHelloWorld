d525eff0aa583acbcc0aaad4fa2404a3a14146f1
samyon196 <samyon196@mail.com>
2020-04-02 14:52:22 +0300

    Hello world svr

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+var cors = require('cors')
+var app = express()
+app.use(cors())
+const port = 3000 | process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening at :${port}`))
